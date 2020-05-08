8f9bb3a0c916c7b3bce928b4527a20530ef86ace
Guetso <legalloisprovensal@gmail.com>
2020-05-08 15:26:54 +0200

    Transition onScroll btn menu

--- anim.js
+++ anim.js
@@ -1,14 +1,15 @@
-// Animation du menu
+// Animation de l'ouverture du menu
 
 const btnMenuOpen = document.querySelector(".menu__btn")
 const btnMenuClose = document.querySelector(".close")
 const toggleMenu = document.getElementById("navpanel")
 
-btnMenuOpen.addEventListener("click", function(){
+btnMenuOpen.addEventListener("click", function () {
     toggleMenu.classList.add("menu--active")
 })
 
-btnMenuClose.addEventListener("click", function(){
+btnMenuClose.addEventListener("click", function () {
     toggleMenu.classList.remove("menu--active")
 })
 
+export {btnMenuOpen}
